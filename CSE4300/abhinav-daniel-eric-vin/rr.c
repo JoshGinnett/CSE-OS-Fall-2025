991c90edca17b5bbcd608566f96f86b91f1985c2
lEric-Wl <eric.wang887@gmail.com>
2025-11-03 21:26:31 -0500

    Worked on rr

--- CSE4300/abhinav-daniel-eric-vin/rr.c
+++ CSE4300/abhinav-daniel-eric-vin/rr.c
@@ -1,14 +1,21 @@
 #include "process.h"
 #include "rr.h"
 
-static time = 0;
-int tq = 1; //time quantum
+static int counter = 0; //Used to keep track of whose turn it is
+const int timeQuantum = 1;
+
+int tq = timeQuantum; //Keeps track of the remaining time quantum for current process
 
 void rr(struct process **procArray, int procArraySize, int globalTime){
-    int turn = time%procArraySize;
+    int turn = counter%procArraySize;
     procArray[turn] -> remainingTime -= 1;
     if(procArray[turn] -> remainingTime <= 0){
-        procArray[turn] -> finishTime = time;
+        procArray[turn] -> finishTime = globalTime;
     }
 
+    tq --;
+    if(tq <= 0){
+        tq = timeQuantum;
+        counter++;
+    }
 }
